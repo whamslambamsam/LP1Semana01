13463e591aec3a311c5a6c987d6eff2f9c72f63f
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-02-20 11:14:40 +0000

    Added character WriteLine commands

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,15 +6,22 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
-            int numeroseis = 6;
-            int numerocinco = 5;
-            int numerotres = 3;
+            int numsix = 6;
+            int numfive = 5;
+            int numthree = 3;
+
+            char charbengali = '\u09D7';
+            char charomega = '\u03A9';
+            char charalpha = '\u03B1';
 
             Console.WriteLine("Hello LP1!");
             
-            Console.WriteLine(numeroseis);
-            Console.WriteLine(numerocinco);
-            Console.WriteLine(numerotres);
+            Console.WriteLine(numsix);
+            Console.WriteLine(numfive);
+            Console.WriteLine(numthree);
+            Console.WriteLine(charbengali);
+            Console.WriteLine(charomega);
+            Console.WriteLine(charalpha);
         }
     }
 }
