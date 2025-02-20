5c9af1122f4b597076b3b4055f5700e2ffbcf67b
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-02-20 11:32:19 +0000

    Added real number WriteLine commands

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -14,14 +14,31 @@ namespace VariousTypes
             char charomega = '\u03A9';
             char charalpha = '\u03B1';
 
-            Console.WriteLine("Hello LP1!");
+            double realpi = 3.1415;
+            double realnum_thirty = 35.66;
+            double realnum_nine = 9.730;
+
+            Console.WriteLine("Hello various Types!");
             
+            Console.WriteLine(" ");
+
             Console.WriteLine(numsix);
             Console.WriteLine(numfive);
             Console.WriteLine(numthree);
+
+            Console.WriteLine(" ");
+
             Console.WriteLine(charbengali);
             Console.WriteLine(charomega);
             Console.WriteLine(charalpha);
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine(realpi);
+            Console.WriteLine(realnum_thirty);
+            Console.WriteLine(realnum_nine);
+
+            Console.WriteLine(" ");
         }
     }
 }
