2c800f00b90d5be4f57d21496b38d39d72e44d7d
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-02-20 10:43:09 +0000

    Added VariousTypes project

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numeroseis = 6;
+            int numerocinco = 5;
+            int numerotres = 3;
+
+            Console.WriteLine("Hello LP1!");
+            
+            Console.WriteLine(numeroseis);
+            Console.WriteLine(numerocinco);
+            Console.WriteLine(numerotres);
+        }
+    }
+}
