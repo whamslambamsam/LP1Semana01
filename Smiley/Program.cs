a206760c4c8a695b11cde857c70521e5d43101e1
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-02-21 17:58:54 +0000

    Added Smiley project, not working and not properly coded

---
+++ Smiley/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Globalization;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input = Console.WriteLine("Insere código\t: ");
+            string code = System.Int32.parse(input, System.Globalization, NumberStyles.None);
+            Console.outputEncoding = Encoding.UTF8;
+            Console.WriteLine(input + code);
+        }
+    }
+}
