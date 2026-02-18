eeac989d17c50fafbfda2b51d68d7b711f918226
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-18 16:37:27 +0000

    Added various types of variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int firstNumber = 3;
+            int secondNumber = 5;
+            char greek = '\u03C1';
+            char star = '\u2605';
+            float floatNum = 45.0f;
+            double doubleNum = 23.9d;
+            bool condition = true;
+            bool falseCondition = false;
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+
+            Console.WriteLine(greek);
+            Console.WriteLine(star);
+
+            Console.WriteLine(floatNum);
+            Console.WriteLine(doubleNum);
+
+            Console.WriteLine(condition);
+            Console.WriteLine(falseCondition);
+        }
+    }
+}
