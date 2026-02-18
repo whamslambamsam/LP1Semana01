4df3aae8cb6651b429247d6e3c243e6054ea99fc
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-18 15:20:49 +0000

    First commit

---
+++ MeuPrograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeuPrograma
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
