cffd0e62809249f6c6d55241c4fbdbf398d4fc7b
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-02-19 16:04:57 +0000

    Added E01 project

---
+++ E01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E01
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
